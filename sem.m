c92bca9e822c0a6738bef7623a1a868f494888f5
Brandon Nanfito <brnanfito@gmail.com>
2024-04-18 18:14:11 -0400

    create sem function

---
+++ sem.m
@@ -0,0 +1,16 @@
+function [out] = sem(x)
+    % input x should be either a vector representing observations of a
+    % single variable or a 2D matrix of observations for multiple variables
+    
+    % if x is a matrix, each row is treated as an observation and each
+    % column as a variable
+    if size(x,1)>1 && size(x,2)>1
+        sig = std(x,[],1);
+        n = size(x,1);
+    else
+        sig = std(x);
+        n = length(x);
+    end
+    out = sig/sqrt(n);
+
+end
