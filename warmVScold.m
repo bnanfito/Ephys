01bc4e769b1a4f19413721797dd597ea37872244
Brandon <bnanfit1@jh.edu>
2024-05-05 12:14:22 -0400

    edits

--- warmVScold.m
+++ warmVScold.m
@@ -6,13 +6,13 @@ anaMode = 'SU';
 if ispc
     dataFold = 'D:\OneDrive - Johns Hopkins\Documents\data';
 elseif ismac
-%     dataFold = '/Users/brandonnanfito/Library/CloudStorage/OneDrive-JohnsHopkins/Documents/data/Ephys';
-    dataFold = '/Users/brandonnanfito/Documents/NielsenLab/data/dataSets/VSS2024';
+%     dataFold = '/Users/brandonnanfito/Library/CloudStorage/OneDrive-JohnsHopkins/Documents/data';
+    dataFold = '/Users/brandonnanfito/Documents/NielsenLab/VSS2024';
 end
 physDir = fullfile(dataFold,'Ephys');
 condNames = {'precool','before','during','after'};
 condColor = {'k','k','c','m'};
-conds = [2,3,4];
+conds = [2,3];
 
 data(1).age = 'P30-32';
 data(1).expList{1,1} =    {'febe7_u000_001'};
@@ -34,9 +34,9 @@ data(3).expList{4,1} =   {'febg9_u000_011';'febg9_u001_008';   'febh5_u000_009'}
 
 data(4).age = 'Adults';
 data(4).expList{1,1} =   {'febe0_u000_000';'febe0_u001_001';                    'febe2_u000_000';   'febf6_u000_000';'febf6_u001_000';                    'febf7_u000_000';                    'febg0_u000_001';'febg0_u001_000';'febg0_u002_000';'febg0_u003_002';                       'febg1_u000_000';'febg1_u001_000';    'febg2_u001_003';                                       'febg3_u000_000'}; %Only pre-any cooling
-data(4).expList{2,1} =   {'febe0_u000_000';'febe0_u001_001';'febe0_u002_000';   'febe2_u000_000';   'febf6_u000_000';'febf6_u001_000';'febf6_u002_000';   'febf7_u000_000';'febf7_u001_001';   'febg0_u000_001';'febg0_u001_000';'febg0_u002_000';'febg0_u003_002';                       'febg1_u000_000';'febg1_u001_000';    'febg2_u001_003';                                       'febg3_u000_000';                 'febg3_u001_011'};
+data(4).expList{2,1} =   {'febe0_u000_000';'febe0_u001_001';'febe0_u002_000';   'febe2_u000_000';   'febf6_u000_000';'febf6_u001_000';                    'febf7_u000_000';'febf7_u001_001';   'febg0_u000_001';'febg0_u001_000';'febg0_u002_000';'febg0_u003_002';                       'febg1_u000_000';'febg1_u001_000';    'febg2_u001_003';                                       'febg3_u000_000';                 'febg3_u001_011'};
 data(4).expList{3,1} =   {                 'febe0_u001_004';'febe0_u002_003';   'febe2_u000_004';                    'febf6_u001_003';'febf6_u002_004';   'febf7_u000_004';                                                                       'febg0_u003_003';                                        'febg1_u001_003';    'febg2_u001_005';'febg2_u001_016';'febg2_u001_020';                      'febg3_u001_003';'febg3_u001_013'};
-data(4).expList{4,1} =   {                                                                                           'febf6_u001_007';'febf6_u002_008';   'febf7_u000_007';'febf7_u001_003';                                                      'febg0_u003_004';'febg0_u004_000';                       'febg1_u001_004';    'febg2_u001_013';'febg2_u001_017';'febg2_u001_021';                      'febg3_u001_004';'febg3_u001_017'};
+data(4).expList{4,1} =   {                                                                                           'febf6_u001_007';'febf6_u002_008';   'febf7_u000_007';'febf7_u001_003';                                                      'febg0_u003_004';                                        'febg1_u001_004';    'febg2_u001_013';'febg2_u001_017';'febg2_u001_021';                      'febg3_u001_004';'febg3_u001_017'};
 
 for ageGroup = 1:4
 
@@ -59,14 +59,9 @@ for e = 1:length(data(ageGroup).expList{c,1})
     elseif strcmp(area,'PSS')
         color = 'r';
     end
-    for p = 1:length(id.probes)
-        if strcmp(id.probes(p).area,area)
-            probe = p;
-        end
-    end
 
 
-    [sumStats,~] = plotUnits(animal,unit,expt,probe,anaMode,'ranksum',0.01,0,0,0,dataFold);
+    [sumStats,~] = plotUnits(animal,unit,expt,area,anaMode,'ranksum',0.01,0,0,0,dataFold);
     close all
     if isempty(sumStats)
         continue
@@ -155,50 +150,54 @@ end
 
 figure;
 for a = 2:4
-    subplot(1,3,a-1,polaraxes);hold on
+    subplot(1,3,a-1);hold on
     clear x y
 for c = conds
 
     x = mean(vertcat(data(a).sums{c}.tuningX{:}));
-%     y = mean(vertcat(data(a).sums{c}.tuningY{:}));
+%     y = vertcat(data(a).sums{c}.tuningY{:});
     for u = 1:height(data(a).sums{c})
 
-        y(u,:) = data(a).sums{c}.tuningY{u}*data(a).sums{c}.rPref(u);
+        y(u,:) = data(a).sums{c}.tuningY{u};
+        y(u,:) = y(u,:)/max(y(u,:));
 
     end
-    y = mean(y);
 
-    sem = std(vertcat(data(a).sums{c}.tuningY{:}))./sqrt(size(vertcat(data(a).sums{c}.tuningY{:}),2));
-    polarplot(deg2rad(x),y,condColor{c},'LineWidth',2);
-    polarplot(deg2rad(repmat(x,2,1)),y+([-1;1]*sem),condColor{c},'LineWidth',2)
+    sem = std(y)./sqrt(size(y,1));
+    plot(x,mean(y),condColor{c},'LineWidth',2);
+    plot(repmat(x,2,1),mean(y)+([-1;1]*sem),condColor{c},'LineWidth',2)
+    title(data(a).age)
 
 end
-% xticks([-180:90:180])
-% ylabel('normalized firing rate')
+xticks([-180:90:180])
+ylabel('normalized firing rate')
 end
 
-figure
+
+
+
+figure;
 for a = 2:4
-subplot(1,3,a-1);hold on
+    subplot(1,3,a-1);hold on
+    clear x y
 for c = conds
-plot(mean(vertcat(data(a).sums{c}.tuningY{:}).*data(a).sums{c}.rPref),condColor{c})
-plot(repmat(9,1,length(data(a).sums{c}.rPref))+(randn(1,length(data(a).sums{c}.rPref))*0.1),data(a).sums{c}.rPref,[condColor{c} '.'])
+
+    x = mean(vertcat(data(a).sums{c}.tuningX{:}));
+    y = vertcat(data(a).sums{c}.tuningY{:});
+    
+
+    sem = std(y)./sqrt(size(y,1));
+    plot(x,mean(y),condColor{c},'LineWidth',2);
+    plot(repmat(x,2,1),mean(y)+([-1;1]*sem),condColor{c},'LineWidth',2)
+    title(data(a).age)
+
 end
+xticks([-180:90:180])
+ylabel('firing rate')
 end
 
 
-% subplot(4,2,8);hold on
-% for c = 1:length(expts)
-% 
-%     x = mean(vertcat(sums{c}.tuningX{:}));
-%     y = mean(vertcat(sums{c}.rPref).*vertcat(sums{c}.tuningY{:}));
-%     sem = std(vertcat(sums{c}.rPref).*vertcat(sums{c}.tuningY{:}))./sqrt(size(vertcat(sums{c}.tuningY{:}),2));
-%     plot(x,y,condColor{c});
-%     plot(repmat(x,2,1),y+([-1;1]*sem),condColor{c})
-% 
-% end
-% xticks([-180:90:180])
-% ylabel('firing rate (Hz)')
+
 
 figure;hold on
 
@@ -214,24 +213,30 @@ for t = 1:3
     for c = conds
         
         if t == 1
-            dist{t} = log2(data(a).sums{c,1}.rPref);
+            dist{t,a,c} = data(a).sums{c,1}.rPref;
             yLbl = 'rPref';
             lims = [1 7 1000];
             offsetScale = 3;
             dpScale = 20;
         elseif t == 2
-            dist{t} = data(a).sums{c,1}.dsi;
+            dist{t,a,c} = data(a).sums{c,1}.dsi;
             yLbl = 'DSI';
             lims = [0 1 1000];
             offsetScale = 3;
             dpScale = 20;
         elseif t == 3
-            dist{t} = 1-data(a).sums{c,1}.dcv;
+            dist{t,a,c} = 1-data(a).sums{c,1}.dcv;
             yLbl = '1-DCV';
             lims = [0 1 1000];
             offsetScale = 3;
             dpScale = 20;
         end
+
+%         cdf = cdfplot(dist{t,a,c});
+%         cdf.Color = condColor{c};
+
+%         histogram(dist{t,a,c},'FaceColor',condColor{c},'FaceAlpha',0.5,'EdgeColor','none')
+
 
         expts = unique(data(a).sums{c,1}.exptName);
         nE = length(expts);
@@ -242,21 +247,19 @@ for t = 1:3
 
         if c == 1
             
-            
-
         elseif c == 2
 
         end
 
         rng = lims(2)-lims(1);
         linWdth = rng/(1*10^1);
-        xBar = mean(dist{t});
-        med = median(dist{t});
-        sem = std(dist{t})/sqrt(length(dist{t}));
-        v = violin(dist{t},0.4,lims,1,0);
+        xBar = mean(dist{t,a,c});
+        med = median(dist{t,a,c});
+        sem = std(dist{t,a,c})/sqrt(length(dist{t,a,c}));
+        v = violin(dist{t,a,c},0.4,lims,1,0);
         z = c*offsetScale;
         s = offsetScale/dpScale;
-        plot(((exptID-(nE/2)-0.5)*s*indAnimals)+z,dist{t}','.','Color',condColor{c});
+        plot(((exptID-(nE/2)-0.5)*s*indAnimals)+z,dist{t,a,c}','.','Color',condColor{c});
     %     plot((vertcat(zeros(size(dist')),ones(size(dist')))*(s))+(z),repmat(dist',2,1),'k');
     %     for dp = 1:length(dist)
     %         plot(((v.g(dp,:)*-1)*(s))+(z),v.gX(1,:),'b')
@@ -269,7 +272,10 @@ for t = 1:3
     %     plot([z z],xBar+([-1 1]*sem),'r')
         
         clear exptID binID
+
+
     end
+
     ylim([lims(1) lims(2)])
     ylabel(yLbl)
     xlim([offsetScale 5*offsetScale])
