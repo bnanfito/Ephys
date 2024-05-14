68a12c14d12e1d0590a300d004d281de4d99b43a
Brandon <bnanfit1@jh.edu>
2024-05-14 01:32:36 -0400

    edits

--- test2.m
+++ test2.m
@@ -1,5 +1,5 @@
 clear all
-% close all
+close all
 
 if ispc
 %     dataFold = 'D:\data'; 
@@ -15,113 +15,61 @@ physDir = fullfile(dataFold,'Ephys');
 dsDir = fullfile(dataFold,'dataSets');
 
 
-% plr = 1;
-% 
-% load(fullfile(dsDir,'training','bidirTrain_MUdataSet.mat'))
-% figure;hold on
-% if plr == 1
-%     sp = subplot(1,3,1,polaraxes); hold on
-%     sp.ThetaZeroLocation = 'top';
-% else
-%     sp = subplot(1,3,1); hold on
-% end
-% for t = 1:4
-%     if t==1
-%         tbl = v1bf{end};
-%         clr = 'b';
-%         linStl = '--';
-%     elseif t==2
-%         tbl = v1af{end};
-%         clr = 'b';
-%         linStl = '-';
-%     elseif t==3
-%         tbl = pssbf{end};
-%         clr = 'r';
-%         linStl = '--';
-%     elseif t==4
-%         tbl = pssaf{end};
-%         clr = 'r';
-%         linStl = '-';
-%     end
-%     tbl = tbl(tbl.goodUnit==1,:);
-%     
-%     x = mean(vertcat(tbl.tuningX{:}));
-%     y = vertcat(tbl.tuningY{:});
-%     Y{t} = mean(y);
-%     X{t} = x;
-%     SEM{t} = std(y)/sqrt(size(y,1));
-%     for u = 1:size(y,1)
-%         y(u,:) = y(u,:)./max(y(u,:));
-%     end
-%     sem = std(y)./sqrt(size(y,1));
-% 
-%     if plr == 1
-%         polarplot(deg2rad(x),mean(y),'Color',clr,'LineStyle',linStl)
-%         polarplot(deg2rad(repmat(x,2,1)),mean(y)+([-1;1]*sem),'Color',clr)
-%     else
-%         plot(x,mean(y),'Color',clr,'LineStyle',linStl)
-%         plot(repmat(x,2,1),mean(y)+([-1;1]*sem),'Color',clr)
-%         xticks([-180 -90 0 90 180])
-%         ylim([0 1])
-%     end
-% 
-% end
-% 
-% 
-% 
-% if plr == 1
-%     sp = subplot(1,3,2,polaraxes); hold on
-%     sp.ThetaZeroLocation = 'top';
-% else
-%     sp = subplot(1,3,2); hold on
-% end
-% for t = 1:4
-%     if t==1
-%         tbl = v1bf{end};
-%         clr = 'b';
-%         linStl = '--';
-%     elseif t==2
-%         tbl = v1af{end};
-%         clr = 'b';
-%         linStl = '-';
-%     elseif t==3
-%         tbl = pssbf{end};
-%         clr = 'r';
-%         linStl = '--';
-%     elseif t==4
-%         tbl = pssaf{end};
-%         clr = 'r';
-%         linStl = '-';
-%     end
-%     tbl = tbl(tbl.goodUnit==1,:);
-% 
-%     if plr == 1
-%         polarplot(deg2rad(X{t}),Y{t},[clr linStl])
-%         polarplot(deg2rad(repmat(X{t},2,1)),Y{t}+([-1;1]*SEM{t}),clr)
-%     else
-%         plot(X{t},Y{t},[clr linStl])
-%         plot(repmat(X{t},2,1),Y{t}+([-1;1]*SEM{t}),clr)
-%         ylim([0 22])
-%     end
-% 
-% end
-% 
-% 
-% 
-% if plr == 1
-%     sp = subplot(1,3,3,polaraxes); hold on
-%     sp.ThetaZeroLocation = 'top';
-% else
-%     sp = subplot(1,3,3); hold on
-% end
-% if plr == 1
-%     polarplot(deg2rad(X{1}),Y{2}./Y{1},'b-o')
-%     polarplot(deg2rad(X{1}),Y{4}./Y{3},'r-o')
-% else
-%     plot(X{1},Y{2}./Y{1},'b-o')
-%     plot(X{1},Y{4}./Y{3},'r-o')
-%     ylim([0 1.5])
-% end
+
+
+
+plr = 0;
+figure; hold on
+for age = 1:3
+
+    if age == 1
+        load(fullfile(dsDir,'P33to34MergeSUDat.mat'))
+    elseif age == 2
+        load(fullfile(dsDir,'P36to37MergeSUDat.mat'))
+    elseif age == 3
+        load(fullfile(dsDir,'adultMergeSUDat.mat'))
+    end
+    fileID = vertcat(dat.tbl.fileID{:});
+    cndID = fileID(:,2);
+
+    for cnd = 1:2
+        idxA = cndID == cnd;
+        data = dat.tbl(idxA,:);
+
+        if cnd == 1
+            clr = 'k';
+        elseif cnd == 2
+            clr = 'c';
+        end
+
+        for u = 1:height(data)
+            x = mean(data.tuningX{u});
+            y = mean(data.tuningY{u});
+            if length(y)<16
+                continue
+            end
+            [X{age,cnd}(u,:),Y{age,cnd}(u,:),~] = alignDirTuning(x,y);
+        end
+
+        subplot(2,3,age); hold on
+        plot(mean(X{age,cnd}),mean(Y{age,cnd}),clr)
+        plot(repmat(mean(X{age,cnd}),2,1) , mean(Y{age,cnd})+([1;-1]*(std(Y{age,cnd})/sqrt(size(Y{age,cnd},1)))) , clr)
+        ylim([-1 10])
+        xticks([-180 -90 0 90 180])
+
+        subplot(2,3,age+3); hold on
+        plot( mean(X{age,cnd}) , mean(Y{age,cnd})/max(mean(Y{age,cnd})) , clr)
+        ylim([-0.2 1])
+        xticks([-180 -90 0 90 180])
+
+        
+    
+    end
+end
+
+
+
+
 
 
 
@@ -141,7 +89,7 @@ for age = 1:3
         clr = [0.6350 0.0780 0.1840]; %red
         linstyl = '-';
     end
-    dist{age} = dat.dsi;
+    dist{age} = 1-dat.dcv;
     dist{age}(dist{age}>1)=1;
 
 %     %SCATTER PLOT
