a3ffabd8d25eb5a936681c5087aab15bcf30f036
Brandon <bnanfit1@jh.edu>
2024-06-17 20:43:29 -0400

    edit MU v1 cooling analysis

--- untitled.m
+++ untitled.m
@@ -1,9 +1,13 @@
 clear all
 close all
 
-load('D:\data\dataSets\V1Cool_Ori16_biff_ranksum.mat','Data','animals','exptList','age')
+load('/Users/brandonnanfito/Documents/NielsenLab/data/dataSets/V1Cool_Ori16_monoChemi_ranksum.mat','Data','animals','exptList','age')
 
-metric = 'rBlank';
+metrics = {'rPref','dsi','ldr'};
+
+for m = 1:length(metrics)
+
+metric = metrics{m};
 
 figure; hold on
 for a = 1:size(Data,1)
@@ -45,13 +49,14 @@ for a = 1:size(Data,1)
         elseif strcmp(metric,'rBlank')
             dist = mean([Data{a,cc}.rBlank{:}],'omitnan');
             xlbl = 'BCFR (Hz)';
-            xlm = [-5 15];
+            xlm = [0 max(dist)];
         end
 
         n(a,cc) = length(dist);
 
         cdf = cdfplot(dist);
         cdf.Color = clr;
+        cdf.LineWidth = 2;
         if strcmp(metric,'rPref')
             cdf = cdfplot(dist2);
             cdf.Color = clr;
@@ -66,5 +71,69 @@ for a = 1:size(Data,1)
 
     end
     title([animal '; P' num2str(age{a})])
+
+end
+sgtitle(metric)
+
+end
+
+
+% figure; hold on
+countU = 0;
+curves = [];
+for a = 1
+
+    for cc = 1:size(Data,2)
+
+        if cc == 1
+            clr = 'k';
+        elseif cc == 2
+            clr = 'c';
+        elseif cc == 3
+            clr = 'm';
+        end
+        
+        curData = Data{a,cc};
+        if isempty(curData)
+           continue
+        end
+        goodInd = curData.goodUnit==1;
+        curData = curData(goodInd,:);
+        if isempty(curData)
+           continue
+        end
+        nU(a,cc) = size(curData,1);
+
+        for u = 1:nU(a,cc)
+            countU = countU + 1;
+            
+            r{countU} = curData.response{u,1};
+            meanR(:,countU) = mean(r{countU},'omitnan');
+            c(:,countU) = curData.condition{u,1};
+            uID(:,countU) = repmat(countU,size(meanR,1),1);
+
+            [cAligned(:,countU),meanRaligned(:,countU),i] = alignDirTuning(c(:,countU)',meanR(:,countU)');
+            rAligned{countU} = r{countU}(:,i);
+            uIDaligned(:,countU) = uID(i,countU);
+
+            x = cAligned(:,countU);
+            y = rAligned{countU};
+            yMean = meanRaligned(:,countU);
+            z = uIDaligned(:,countU);
+
+%             plot3(x,z,yMean,clr)
+
+            figure;hold on;
+            plot(x,y',[clr '.'])
+            plot(x,yMean,clr,'LineWidth',2)
+
+
+
+        end
+
+    end
+    
+
+    
 
 end
