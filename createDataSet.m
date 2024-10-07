2d4e8592f324d9716e52aa16ea868d91f4f8bf51
Brandon Nanfito <brnanfito@gmail.com>
2024-10-07 16:39:20 -0400

    edits

--- createDataSet.m
+++ createDataSet.m
@@ -17,6 +17,7 @@ ageLims = [0 1000];
 coolAnimals = unique(coolData.experimentId);
 cntrlIdx = ismember(cntrlData.experimentId,coolAnimals);
 cntrlData = cntrlData(cntrlIdx,:);
+clear cntrlIdx
 
 %% remove Kalatsky experiments
 coolKalIdx = strcmp(coolData.module,'BK');
@@ -27,62 +28,46 @@ cntrlKalIdx = strcmp(cntrlData.module,'BK');
 cntrlKalatskyData = cntrlData(cntrlKalIdx,:);
 cntrlData = cntrlData(~cntrlKalIdx,:);
 
-%% remove fullfield experiments
-% include experiments with size as a looped parameter (should have both ff and hemifield trials)
-hemiCoolIdx = contains(coolData.looperNameCond1,'size') | ...
-           contains(coolData.looperNameCond2,'size') | ...
-           contains(coolData.looperNameCond3,'size') | ...
-           contains(coolData.looperNameCond4,'size') | ...
-           contains(coolData.looperNameCond5,'size');
-stimParams = {'x_size','y_size','x_pos','y_pos'};
-for i = 1:height(coolData)
-    fileBase = [coolData.experimentId{i} '_u' coolData.unitNr{i} '_' coolData.experimentNr{i}];
-    load(fullfile(anaPath,fileBase(1:5),[fileBase '.analyzer']),'-mat')
-    spValCool(i,1) = getparam(stimParams{1},Analyzer);
-    spValCool(i,2) = getparam(stimParams{2},Analyzer);
-    spValCool(i,3) = getparam(stimParams{3},Analyzer);
-    spValCool(i,4) = getparam(stimParams{4},Analyzer);
-end
-hemiCoolIdx = hemiCoolIdx | spValCool( : , contains(stimParams,'x_size') ) == 50 | ...
-                    spValCool( : , contains(stimParams,'y_size') ) == 50;
-ffCoolData = coolData(~hemiCoolIdx,:);
-coolData = coolData(hemiCoolIdx,:);
-
-hemiCntrlIdx = contains(cntrlData.looperNameCond1,'size') | ...
-           contains(cntrlData.looperNameCond2,'size') | ...
-           contains(cntrlData.looperNameCond3,'size') | ...
-           contains(cntrlData.looperNameCond4,'size') | ...
-           contains(cntrlData.looperNameCond5,'size');
-for i = 1:height(cntrlData)
-    fileBase = [cntrlData.experimentId{i} '_u' cntrlData.unitNr{i} '_' cntrlData.experimentNr{i}];
-    load(fullfile(anaPath,fileBase(1:5),[fileBase '.analyzer']),'-mat')
-    spValCntrl(i,1) = getparam(stimParams{1},Analyzer);
-    spValCntrl(i,2) = getparam(stimParams{2},Analyzer);
-    spValCntrl(i,3) = getparam(stimParams{3},Analyzer);
-    spValCntrl(i,4) = getparam(stimParams{4},Analyzer);
-end
-hemiCntrlIdx = hemiCntrlIdx | spValCntrl( : , contains(stimParams,'x_size') ) == 50 | ...
-                    spValCntrl( : , contains(stimParams,'y_size') ) == 50;
-ffCntrlData = cntrlData(~hemiCntrlIdx,:);
-cntrlData = cntrlData(hemiCntrlIdx,:);
-
-
-%% split datasets by domval
+%% index experiments
 
 coolLooperVals = [coolData.looperNameCond1 coolData.looperNameCond2 ...
                   coolData.looperNameCond3 coolData.looperNameCond4 ...
                   coolData.looperNameCond5];
-
 cntrlLooperVals = [cntrlData.looperNameCond1 cntrlData.looperNameCond2 ...
                    cntrlData.looperNameCond3 cntrlData.looperNameCond4 ...
                    cntrlData.looperNameCond5];
 
-%contrast dataset
-cntrlContIdx = sum(contains(cntrlLooperVals,'contrast'),2)>0;
-cntrlContData = cntrlData(cntrlContIdx,:);
+% contralateral hemifield experiments
+stimParams = {'x_size','y_size','x_pos','y_pos'};
 
+hemiCoolIdx = sum(contains('size',coolLooperVals),2)>0;
+for i = 1:height(coolData)
+    fileBase = [coolData.experimentId{i} '_u' coolData.unitNr{i} '_' coolData.experimentNr{i}];
+    load(fullfile(anaPath,fileBase(1:5),[fileBase '.analyzer']),'-mat')
+    for p = 1:length(stimParams)
+        spValCool(i,p) = getparam(stimParams{p},Analyzer);
+    end
+end
+hemiCoolIdx = hemiCoolIdx | spValCool( : , contains(stimParams,'x_size') ) == 50 | ...
+                    spValCool( : , contains(stimParams,'y_size') ) == 50;
+
+
+hemiCntrlIdx = sum(contains('size',cntrlLooperVals),2)>0;
+for i = 1:height(cntrlData)
+    fileBase = [cntrlData.experimentId{i} '_u' cntrlData.unitNr{i} '_' cntrlData.experimentNr{i}];
+    load(fullfile(anaPath,fileBase(1:5),[fileBase '.analyzer']),'-mat')
+    for p = 1:length(stimParams)
+        spValCntrl(i,p) = getparam(stimParams{p},Analyzer);
+    end
+end
+hemiCntrlIdx = hemiCntrlIdx | spValCntrl( : , contains(stimParams,'x_size') ) == 50 | ...
+                    spValCntrl( : , contains(stimParams,'y_size') ) == 50;
+
+
+% contrast experiments
 coolContIdx = sum(contains(coolLooperVals,'contrast'),2)>0;
-coolContData = coolData(coolContIdx,:);
+cntrlContIdx = sum(contains(cntrlLooperVals,'contrast'),2)>0;
+
 
 
 
