e9277a46c2ea7114d2665c67aecfde0ef3ac30a0
Brandon Nanfito <brnanfito@gmail.com>
2024-10-10 10:57:57 -0400

    edits

--- createDataSet.m
+++ createDataSet.m
@@ -40,7 +40,7 @@ cntrlLooperVals = [cntrlData.looperNameCond1 cntrlData.looperNameCond2 ...
 % contralateral hemifield experiments
 stimParams = {'x_size','y_size','x_pos','y_pos'};
 
-hemiCoolIdx = sum(contains('size',coolLooperVals),2)>0;
+hemiCoolIdx = sum(contains(coolLooperVals,'size'),2)>0;
 for i = 1:height(coolData)
     fileBase = [coolData.experimentId{i} '_u' coolData.unitNr{i} '_' coolData.experimentNr{i}];
     load(fullfile(anaPath,fileBase(1:5),[fileBase '.analyzer']),'-mat')
@@ -52,7 +52,7 @@ hemiCoolIdx = hemiCoolIdx | spValCool( : , contains(stimParams,'x_size') ) == 50
                     spValCool( : , contains(stimParams,'y_size') ) == 50;
 
 
-hemiCntrlIdx = sum(contains('size',cntrlLooperVals),2)>0;
+hemiCntrlIdx = sum(contains(cntrlLooperVals,'size'),2)>0;
 for i = 1:height(cntrlData)
     fileBase = [cntrlData.experimentId{i} '_u' cntrlData.unitNr{i} '_' cntrlData.experimentNr{i}];
     load(fullfile(anaPath,fileBase(1:5),[fileBase '.analyzer']),'-mat')
@@ -67,6 +67,11 @@ hemiCntrlIdx = hemiCntrlIdx | spValCntrl( : , contains(stimParams,'x_size') ) ==
 % contrast experiments
 coolContIdx = sum(contains(coolLooperVals,'contrast'),2)>0;
 cntrlContIdx = sum(contains(cntrlLooperVals,'contrast'),2)>0;
+
+% ori expts
+coolOriIdx = sum(contains(coolLooperVals,{'ori16','ori12'}),2)>0;
+cntrlOriIdx = sum(contains(cntrlLooperVals,{'ori16','ori12'}),2)>0;
+
 
 
 
