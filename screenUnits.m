5fcd0d32bec27131097c6e259ff97765f4a9dca1
Brandon <bnanfit1@jh.edu>
2025-04-11 21:19:59 -0400

    remove goodUnit column from sumStats
    instead of assigning goodUnits in sumStats tables, now the pVis column contains a pvalue for 3 different statistical tests (generated by visTest.m) and the goodUnit assignment for a sumStats table can still be generated by screenUnits.m but will not be included in the table

--- screenUnits.m
+++ screenUnits.m
@@ -1,68 +1,21 @@
-function [out,pVis] = screenUnits(sumStats,mode,blankBit,visTest,alpha)
-
-% INPUTS
-% 1. in = structure containing spike data organized by unit.
-% 2. mode = 'SU' for single-unit analysis or 'MU' for multi-unit analysis
-% 3. blankBit = binary vector (0 or 1) indicating which condition in the nRep x nCondition firing rate matrix (field in 'in') is a blank (=1)
-
-% OUTPUTS
-% out = nUnits long binary vector (0 or 1) indicating which pass inclusion criteria (=1) and which do not (=0)
-% pVis = vector of p values for each unit, calculated from hypothesis test (visTest)
+function [out] = screenUnits(sumStats,anaMode)
  
-% alpha = 0.01;
-% visTest = 'ranksum';
-
+alpha = 0.01;
 for u = 1:height(sumStats)
 
-    if sum(contains(sumStats.paramKey{u},'size'))>0
-        cndSize = sumStats.cndKey{u}(:,contains(sumStats.paramKey{u},'size'));
-        sizes = unique(cndSize);
-        if length(sizes)>1
-            a = sumStats.response{u};
-            b = sumStats(u,:).fr.bc(:,cndSize>100);
-            cmpre = a==b;
-            cmpre = cmpre(~isnan(a) & ~isnan(b));
-            isFF = length(unique(cmpre)) == 1 & max(unique(cmpre)) == 1;
-            if isFF
-                cndInclude = cndSize==max(sizes);
-            else
-                cndInclude = cndSize==min(sizes);
-            end
-        else
-            cndInclude = ~blankBit;
-        end
-    else
-        cndInclude = ~blankBit;
+    isVis = sumStats.pVis(u).anova<alpha;
+    if strcmp(anaMode,'SU')
+        isAct = sumStats.rPref(u)>=2;
+        isSU = strcmp(sumStats{u}.uInfo,'SU');
+        maybeSU = strcmp(sumStats{u}.uInfo,'SU?');
+        out(u) = isVis & isAct & isSU;
+%         out(u) = isVis & isAct & (isSU | maybeSU);
+    elseif strcmp(anaMode,'MU')
+        isAct = sumStats.rPref(u)>=2;
+        out(u) = isVis & isAct;
     end
 
-    baseFR = sumStats(u,:).fr.base(:,cndInclude);    baseFR = baseFR(:); 
-    stimFR = sumStats(u,:).fr.stim(:,cndInclude);    stimFR = stimFR(:);
-    bcFR = sumStats(u,:).fr.bc(:,cndInclude);        bcFR = bcFR(:); bcFR = bcFR(~isnan(bcFR));
- 
-    if strcmp(visTest,'signrank')
-        pVis(u,1) = signrank(bcFR);
-    elseif strcmp(visTest,'ranksum')
-        pVis(u,1) = ranksum(baseFR,stimFR);
-    elseif strcmp(visTest,'anova')
-        pVis(u,1) = anova1([sumStats.response{u},sumStats.rBlank{u}],[],'off');
-    end
-    isVis(u) = pVis(u,1)<alpha;
-
-    if strcmp(mode,'SU')
-        isAct(u) = sumStats.rPref(u)>=2;
-        isSU(u) = strcmp(sumStats(u,:).uInfo,'SU');
-        maybeSU(u) = strcmp(sumStats(u,:).uInfo,'SU?');
-    elseif strcmp(mode,'MU')
-        isAct(u) = sumStats.rPref(u)>=2;
-    end
-
-end
-
-if strcmp(mode,'SU')
-    out = isVis & isAct & isSU;
-    %     out = isVis & isAct & (isSU | maybeSU);
-elseif strcmp(mode,'MU')
-    out = isVis & isAct;
+    clear isVis isAct isSU maybeSU
 end
 
 
