65a06100b8bf646593ad8d09b8767cf9377b16f2
Brandon <bnanfit1@jh.edu>
2026-03-03 01:29:30 -0500

    add anaDSdev_circRegress

---
+++ anaDSdev_circRegress.m
@@ -0,0 +1,100 @@
+clear all
+close all
+
+%% Load data
+
+anaMode = 'SU';
+
+% dataFold = '/Volumes/Lab drive/Brandon/data/dataSets/DSdev';
+% dataFold = '/Users/brandonnanfito/Documents/NielsenLab/data/dataSets/DSdev';
+dataFold = '/Volumes/NielsenHome2/Brandon/data/dataSets/DSdev';
+% dataFold = 'F:\Brandon\data\dataSets\DSdev';
+% dataFold = 'Y:\Brandon\data\dataSets\DSdev';
+load(fullfile(dataFold,['DSdev_' anaMode 'dataSet.mat']))
+
+areas = {'V1','PSS'};
+ageGroups = {[29 32],[33 36],[37 max(projTbl.age)]};
+% ageGroups = {[28 32],[29 33],[30 34],[31 35],[32 36],[33 37],[34 38],[35 39],[36 40],[37 41],[38 42],[39 43],[40 44],[41 300]};
+nAG = length(ageGroups);
+nAR = length(areas);
+
+for ar = 1:nAR
+    for ag = 1:nAG
+
+        r = rMat{ar,ag}.rTrial_norm;
+        tDir = rMat{ar,ag}.cTrial;
+        tOri = mod(tDir,180);
+        tD = [cos(deg2rad(tDir)) sin(deg2rad(tDir))];
+        tO = [cos(2*deg2rad(tOri)) sin(2*deg2rad(tOri))];
+        [coeff,score] = pca(r);
+        for nPC = 1:10
+            
+            nFold = 5;
+            cv = cvpartition(length(tDir),'KFold',5);    
+            errDir = zeros(1,nFold);
+            errOri = zeros(1,nFold);
+            for fold = 1:nFold
+            
+                trainIdx = training(cv,fold);
+                testIdx  = test(cv,fold);
+                cDir = score(trainIdx,1:nPC) \ tD(trainIdx,:); % learn mapping coeffs
+                cOri = score(trainIdx,1:nPC) \ tO(trainIdx,:);
+                tD_pred = score(testIdx,1:nPC)*cDir; % project PCs into 2D embedding
+                tDir_pred = rad2deg(mod(atan2(tD_pred(:,2),tD_pred(:,1)),2*pi));
+                tO_pred = score(testIdx,1:nPC)*cOri;
+                tOri_pred = rad2deg(mod(atan2(tO_pred(:,2),tO_pred(:,1)),2*pi)); %this is still in a modulo 360 circle, must be divided by two later
+                
+                % diffDir = diff([tDir(testIdx) tDir_pred]');
+                % diffOri = diff([2*tOri(testIdx) tOri_pred]')/2;
+                diffDir = rad2deg(angdiff(deg2rad([tDir(testIdx) tDir_pred])'));
+                diffOri = rad2deg(angdiff(deg2rad([2*tOri(testIdx) tOri_pred])'))/2;
+
+                errDir(fold) = mean(abs(diffDir));
+                errOri(fold) = mean(abs(diffOri));
+            
+            end
+            
+            mseDir{ar,ag}(nPC) = mean(errDir);
+            mseOri{ar,ag}(nPC) = mean(errOri);
+        
+        end
+
+    end
+end
+
+figure;
+subplot(2,2,1);hold on
+plot(tD(:,1),tD(:,2),'o')
+plot(tD_pred(:,1),tD_pred(:,2),'r*')
+plot(tD(testIdx,1),tD(testIdx,2),'g*')
+plot([tD(testIdx,1) tD_pred(:,1)]',[tD(testIdx,2) tD_pred(:,2)]','k')
+[x,y] = pol2cart(repmat(deg2rad(tDir_pred),1,2)',[zeros(length(tDir_pred),1) ones(length(tDir_pred),1)]');
+plot(x,y,'r')
+axis square
+title('direction')
+subplot(2,2,2);hold on
+plot(tO(:,1),tO(:,2),'o')
+plot(tO_pred(:,1),tO_pred(:,2),'r*')
+plot(tO(testIdx,1),tO(testIdx,2),'g*')
+plot([tO(testIdx,1) tO_pred(:,1)]',[tO(testIdx,2) tO_pred(:,2)]','k')
+[x,y] = pol2cart(repmat(deg2rad(tOri_pred),1,2)',[zeros(length(tOri_pred),1) ones(length(tOri_pred),1)]');
+plot(x,y,'r')
+axis square
+title('orientation')
+colororder({'k','g'})
+for ag = 1:nAG
+    subplot(2,3,nAG+ag);hold on
+    yyaxis left
+    plot(mseDir{1,ag},'k--')
+    plot(mseDir{2,ag},'k-')
+    ylabel('direction error (mod 360)')
+    ylim([0 180])
+    yyaxis right
+    plot(mseOri{1,ag},'g--')
+    plot(mseOri{2,ag},'g-')
+    ylabel('orientation error (mod 180)')
+    ylim([0 90])
+    if ag == 1
+        legend({'V1 dir','PMLS dir','V1 ori','PMLS ori'})
+    end
+end
