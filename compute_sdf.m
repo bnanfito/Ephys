372bad0b4f2a31164dd903c0752e662a4568dd45
Brandon Nanfito <brnanfito@gmail.com>
2026-07-09 16:00:14 -0400

    fix bug in compute_sdf
    fix indexing of condition and trial; this issue became apparent when trying to use with full field analysis which index the second half of conditions

--- compute_sdf.m
+++ compute_sdf.m
@@ -2,29 +2,27 @@ function [SDF] = compute_sdf(data,varargin)
 
 spkTimes = data.spkTimes{1};
 conds = data.condition{1}(strcmp(data.paramKey{1},'ori'),:);
-cPref = find( conds == data.oriPref(1) );
-cNull = find( conds == data.oriNull(1) );
-cOrth1 = find( conds == mod(conds(cPref)+90,360) );
-cOrth2 = find( conds == mod(conds(cPref)-90,360) );
 nConds = length(conds);
 if length(data.paramKey{1})>1
     rA = data.response{1}; rA = rA(~isnan(rA));
     rB = data.fr(1).bc(:,1:nConds); rB= rB(~isnan(rB));
     rC = data.fr(1).bc(:,(1:nConds)+nConds); rC = rC(~isnan(rC));
-    if unique(rB == rA)==1
+    if length(rB)==length(rA) && unique(rB==rA)==1
         cndsInclude = 1:nConds;
-    elseif unique(rC == rA)==1
+    elseif length(rC)==length(rA) && unique(rC==rA)==1
         cndsInclude = (1:nConds)+nConds;
     end
 else
     cndsInclude = 1:nConds;
 end
-trialKey = data.fr(1).trialNum(1:5,cndsInclude);
-trialInclude = sort(trialKey(:));
+nReps = 5;
+trialKey = data.fr(1).trialNum(1:nReps,cndsInclude); trialKey = trialKey(:);
+condKey = data.fr(1).trialCond(1:nReps,cndsInclude); condKey = condKey(:);
+repKey = repmat((1:nReps)',1,nConds); repKey = repKey(:);
 nTrials = numel(trialKey);
-nReps = size(trialKey,1);
-repKey = repmat((1:nReps)',1,nConds);
-condKey = repmat(1:nConds,nReps,1);
+cPref = cndsInclude( conds == data.oriPref(1) );
+cNull = cndsInclude( conds == data.oriNull(1) );
+
 
 spkT = spkTimes(1,:);
 trialId = spkTimes(2,:);
@@ -32,7 +30,7 @@ dt = 0.001;
 time = -1:dt:2;
 w = 0.020;
 for t = 1:nTrials
-    tIdx = trialInclude(t);
+    tIdx = trialKey(t);
     if ismember(tIdx,trialId)
         xT = spkT(trialId==tIdx);
         for s = 1:length(xT)
@@ -45,8 +43,8 @@ for t = 1:nTrials
     end
 end
 for c = cndsInclude
-    sdf_cMean(c,:) = mean( sdf(:,ismember(1:nTrials,trialKey(:,c)))' ,'omitnan');
-    sdf_cSem(c,:) = sem( sdf(:,ismember(1:nTrials,trialKey(:,c)))' );
+    sdf_cMean(c,:) = mean( sdf(:,condKey==c)' ,'omitnan');
+    sdf_cSem(c,:) = sem( sdf(:,condKey==c)' );
 end
 
 SDF.time = time;
@@ -73,10 +71,11 @@ if ~isempty(varargin) && varargin{1}==1
     
     ax(2) = nexttile;hold on
     for c = 1:nConds
-        patch([0 1 1 0],[0 0 1 1]+(c-1),clrs(c,:),'EdgeColor','none','FaceAlpha',0.2)
+        cIdx = cndsInclude(c);
+        patch([0 1 1 0],[0 0 1 1]+(cIdx-1),clrs(c,:),'EdgeColor','none','FaceAlpha',0.2)
     end
     for t = 1:nTrials
-        tIdx = trialInclude(t);
+        tIdx = trialKey(t);
         xT = spkT(trialId==tIdx);
         if isempty(xT)
             continue
@@ -87,7 +86,7 @@ if ~isempty(varargin) && varargin{1}==1
         plot(xT,yT, 'k.')
     end
     xlim([min(SDF.time) max(SDF.time)])
-    ylim([0 nConds])
+    ylim([min(cndsInclude) max(cndsInclude)])
     
     ax(3) = nexttile;hold on
     % plot(nT,[g{:}]/max([g{:}],[],'all'))
@@ -96,8 +95,8 @@ if ~isempty(varargin) && varargin{1}==1
     
     ax(4) = nexttile;hold on
     for c = [cPref,cNull]
-        plot(SDF.time,SDF.cMean(c,:),'Color',clrs(c,:),'LineWidth',1)
-        patch([SDF.time fliplr(SDF.time)],[SDF.cMean(c,:)+SDF.cSem(c,:) fliplr(SDF.cMean(c,:)-SDF.cSem(c,:))],clrs(c,:),'EdgeColor','none','FaceAlpha',0.2)
+        plot(SDF.time,SDF.cMean(c,:),'Color',clrs(c-(min(cndsInclude)-1),:),'LineWidth',1)
+        patch([SDF.time fliplr(SDF.time)],[SDF.cMean(c,:)+SDF.cSem(c,:) fliplr(SDF.cMean(c,:)-SDF.cSem(c,:))],clrs(c-(min(cndsInclude)-1),:),'EdgeColor','none','FaceAlpha',0.2)
     end
     linkaxes(ax,'x')
 end
