cf16e6f3cdcc59f4eb6e1b9cdf71da4e8a9dac89
Brandon Nanfito <brnanfito@gmail.com>
2026-08-04 16:13:05 -0400

    edit anaDSdev_decode
    add shuffle

--- anaDSdev_decode.m
+++ anaDSdev_decode.m
@@ -1,5 +1,5 @@
 clear all
-% close all
+close all
 
 %% Load data
 
@@ -24,10 +24,13 @@ if strcmp(anaMode,'SU')
 elseif strcmp(anaMode,'MU')
     nU = 300;
 end
-nPC = nU;
+nPC = 30;
 
-ar = 2;
-ag = 1;
+T2toT1 = @(in) rad2deg( mod(atan2(in(:,2),in(:,1)),2*pi) );
+calcErr = @(in1,in2) rad2deg( angdiff(deg2rad( [in1,in2]' )) );
+
+for ar = 1:nAR
+for ag = 1:nAG
 
 for b = 1:nBoot
     
@@ -37,73 +40,111 @@ for b = 1:nBoot
     T1_dir = rMat{ar,ag}.cTrial;
     T1_dirShuff = T1_dir(randperm(length(T1_dir),length(T1_dir)));
     T1_ori = mod(T1_dir,180);
+    T1_oriShuff = T1_ori(randperm(length(T1_ori),length(T1_ori)));
 
     T2_dir = [cos(deg2rad(T1_dir)) sin(deg2rad(T1_dir))]; %
     T2_dirShuff = [cos(deg2rad(T1_dirShuff)) sin(deg2rad(T1_dirShuff))];
     T2_ori = [cos(2*deg2rad(T1_ori)) sin(2*deg2rad(T1_ori))]; % multiply by 2 so that modulo 180 maps onto a unit circle instead of a semi-circle
+    T2_oriShuff = [cos(deg2rad(T1_oriShuff)) sin(deg2rad(T1_oriShuff))];
 
     for p = 1:nPC
-        pcs = p;
+        pcs = 1:p;
         mae_dir = nan(1,nFold);
+        mae_dirShuff = nan(1,nFold);
         mae_ori = nan(1,nFold);
+        mae_oriShuff = nan(1,nFold);
 
         for f = 1:nFold
         
             cv = cvpartition(size(R,1),"KFold",nFold);
             trainIdx = training(cv,f);
             testIdx = test(cv,f);
-            [cof,scr,~,~,~,mu] = pca(R(trainIdx,:));
+            [cof,scr_train,~,~,~,mu] = pca(R(trainIdx,:));
+            scr_test = ((R(testIdx,:)-mu)*cof(:,pcs));
 
-            B_dir = scr(:,pcs)\T2_dir(trainIdx,:);
-            T2pred_dir = ((R(testIdx,:)-mu)*cof(:,pcs))*B_dir;
-            T2train_dir = ((R(trainIdx,:)-mu)*cof(:,pcs))*B_dir;
-            T1pred_dir = rad2deg( mod(atan2(T2pred_dir(:,2),T2pred_dir(:,1)),2*pi) );
-            err_dir = rad2deg(angdiff(deg2rad([T1_dir(testIdx),T1pred_dir]')));
+            B_dir = scr_train(:,pcs)\T2_dir(trainIdx,:);
+            T2pred_dir = scr_test*B_dir;
+            T1pred_dir = T2toT1(T2pred_dir);
+            err_dir = calcErr(T1_dir(testIdx),T1pred_dir);
             mae_dir(f) = mean(abs(err_dir));
+
+            B_dirShuff = scr_train(:,pcs)\T2_dirShuff(trainIdx,:);
+            T2pred_dirShuff = scr_test*B_dirShuff;
+            T1pred_dirShuff = T2toT1(T2pred_dirShuff);
+            err_dirShuff = calcErr(T1_dirShuff(testIdx),T1pred_dirShuff);
+            mae_dirShuff(f) = mean(abs(err_dirShuff));
     
-            B_ori = scr(:,pcs)\T2_ori(trainIdx,:);
-            T2pred_ori = ((R(testIdx,:)-mu)*cof(:,pcs))*B_ori;
-            T1pred_ori = rad2deg( mod(atan2(T2pred_ori(:,2),T2pred_ori(:,1)),2*pi) );
-            err_ori = rad2deg(angdiff(deg2rad([2*T1_ori(testIdx),T1pred_ori]')))/2;
+            B_ori = scr_train(:,pcs)\T2_ori(trainIdx,:);
+            T2pred_ori = scr_test*B_ori;
+            T1pred_ori = T2toT1(T2pred_ori);
+            err_ori = calcErr(2*T1_ori(testIdx),T1pred_ori)/2;
             mae_ori(f) = mean(abs(err_ori));
+
+            B_oriShuff = scr_train(:,pcs)\T2_oriShuff(trainIdx,:);
+            T2pred_oriShuff = scr_test*B_oriShuff;
+            T1pred_oriShuff = T2toT1(T2pred_oriShuff);
+            err_oriShuff = calcErr(2*T1_oriShuff(testIdx),T1pred_oriShuff)/2;
+            mae_oriShuff(f) = mean(abs(err_oriShuff));
         
-            if b==1 && p==4 && f==1
-                figure
-                plot(T2_dir(:,2),T2_dir(:,1),'ko');hold on
-                plot([T2_dir(testIdx,2) T2pred_dir(:,2)]',[T2_dir(testIdx,1) T2pred_dir(:,1)]','r-')
-                plot(T2pred_dir(:,2),T2pred_dir(:,1),'r*')
-%                 plot([T2_dir(trainIdx,2) T2train_dir(:,2)]',[T2_dir(trainIdx,1) T2train_dir(:,1)]','g-')
-%                 plot(T2train_dir(:,2),T2train_dir(:,1),'g*')
-                axis square
-                
-                figure
-                plot(T2_ori(:,2),T2_ori(:,1),'ko');hold on
-                plot(T2pred_ori(:,2),T2pred_ori(:,1),'r*')
-                plot([T2_ori(testIdx,2) T2pred_ori(:,2)]',[T2_ori(testIdx,1) T2pred_ori(:,1)]','r-')
-                axis square
-            end
+%             if b==1 && p==1 && f==1
+%                 figure
+%                 plot(T2_dir(:,2),T2_dir(:,1),'ko');hold on
+%                 plot([T2_dir(testIdx,2) T2pred_dir(:,2)]',[T2_dir(testIdx,1) T2pred_dir(:,1)]','r-')
+%                 plot(T2pred_dir(:,2),T2pred_dir(:,1),'r*')
+%                 axis square
+%                 
+%                 figure
+%                 plot(T2_ori(:,2),T2_ori(:,1),'ko');hold on
+%                 plot(T2pred_ori(:,2),T2pred_ori(:,1),'r*')
+%                 plot([T2_ori(testIdx,2) T2pred_ori(:,2)]',[T2_ori(testIdx,1) T2pred_ori(:,1)]','r-')
+%                 axis square
+%             end
 
         end
 
-        MAE_dir(b,p) = mean(mae_dir); % compute mean mae across boots    
-        MAE_ori(b,p) = mean(mae_ori);
+        MAE_dir{ar,ag}(b,p) = mean(mae_dir); % compute mean mae across folds    
+        MAE_dirShuff{ar,ag}(b,p) = mean(mae_dirShuff);
+        MAE_ori{ar,ag}(b,p) = mean(mae_ori);
+        MAE_oriShuff{ar,ag}(b,p) = mean(mae_oriShuff);
 
     end
 
 end
 
+end
+end
+
 %% plotting
 
 
-figure
-yyaxis left
-plot(mean(MAE_dir))
-ylim([40 120])
-xlim([0 nPC])
-% patch([1:nPC fliplr(1:nPC)],[MAE_dirCI(1,:) fliplr(MAE_dirCI(2,:))],'k','EdgeColor','none','FaceAlpha',0.2)
-yyaxis right
-plot(mean(MAE_ori))
-ylim([20 60])
-xlim([0 nPC])
-% patch([1:nPC fliplr(1:nPC)],[MAE_oriCI(1,:) fliplr(MAE_oriCI(2,:))],'k','EdgeColor','none','FaceAlpha',0.2)
+figure; 
 
+for ar = 1:nAR
+for ag = 1:nAG
+    subplot(nAR,nAG,ag+(nAG*(ar-1))); hold on
+    
+    yyaxis left
+    y = mean(MAE_dir{ar,ag});
+    err = confInt(MAE_dir{ar,ag});
+    plot(y,'-o')
+    patch([1:nPC fliplr(1:nPC)],[err(1,:) fliplr(err(2,:))],'k','EdgeColor','none','FaceAlpha',0.2)
+    y = mean(MAE_dirShuff{ar,ag});
+    err = confInt(MAE_dirShuff{ar,ag});
+    plot(y)
+    patch([1:nPC fliplr(1:nPC)],[err(1,:) fliplr(err(2,:))],'r','EdgeColor','none','FaceAlpha',0.2)
+    ylim([40 120])
+    xlim([0 nPC])
+    
+    yyaxis right
+    y = mean(MAE_ori{ar,ag});
+    err = confInt(MAE_ori{ar,ag});
+    plot(y,'-o')
+    patch([1:nPC fliplr(1:nPC)],[err(1,:) fliplr(err(2,:))],'k','EdgeColor','none','FaceAlpha',0.2)
+    y = mean(MAE_oriShuff{ar,ag});
+    err = confInt(MAE_oriShuff{ar,ag});
+    plot(y)
+    patch([1:nPC fliplr(1:nPC)],[err(1,:) fliplr(err(2,:))],'r','EdgeColor','none','FaceAlpha',0.2)
+    ylim([20 60])
+    xlim([0 nPC])
+end
+end
